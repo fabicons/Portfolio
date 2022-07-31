57db3490513e2eb5927a032c880a718aea5b0ffc
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-07-31 14:56:31 +0100

    Navbar: Burger Menu: add: logic

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
-import React from 'react'
+import React, {useState} from 'react'
 import Link from 'next/link'
 import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
 import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 const Navbar = () => {
+    
+    const [nav, setNav] = useState(false)
+    const handleNav= () => {
+        setNav(!nav)
+    }
+
   return (
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
         <div className='flex justify-between items-center h-full px-2 2xl:px-16'>
@@ -33,17 +39,23 @@ const Navbar = () => {
                     </Link>
                 </ul>
                 {/* Burger Menu */}
-                <div className='md:hidden'>
+                <div onClick={handleNav} className='md:hidden'>
                     <AiOutlineMenu size={25}/>
                 </div>
             </div>
         </div>
-        <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
-            <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+        <div className = {nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+            <div 
+                className= {
+                    nav 
+                    ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' 
+                    : 'fixed left-[-100%] top-0  p-10 ease-in duration-500'
+                    }
+            >
                 <div>
                     <div className='flex w-full items-center justify-between'>
                         <Image src='/../public/assets/navLogo.png' width ='87' height='35' alt='/'/>
-                        <div className= 'rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                        <div onClick={handleNav} className= 'rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
                             <AiOutlineClose/>
                         </div>
                     </div>
