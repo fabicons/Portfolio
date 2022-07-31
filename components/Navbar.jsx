75efeee50fd5dce846779784df77e726873ae0a7
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-07-31 14:16:54 +0100

    Navbar: burger menu add: styling & responsive sizing

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import Link from 'next/link'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 const Navbar = () => {
   return (
@@ -11,27 +12,47 @@ const Navbar = () => {
             alt="/" 
             width="125" 
             height="50"/>
+            <div>
+                <ul className='hidden md:flex'>
+                    <Link href="/">
+                    <li className ='ml10 text sm uppercase hover:border-b' >Home</li>
+                    </Link>
+                    <Link href="/">
+                    <li className ='ml10 text sm uppercase hover:border-b' >About</li>
+                    </Link>
+                    <Link href="/">
+                    <li className ='ml10 text sm uppercase hover:border-b' >Skills</li>
+                    </Link>
+                    <Link href="/">
+                    <li className ='ml10 text sm uppercase hover:border-b' >Projects</li>
+                    </Link>
+                    <Link href="/">
+                    <li className ='ml10 text sm uppercase hover:border-b' >Contact</li>
+                    </Link>
+                </ul>
+                {/* Burger Menu */}
+                <div className='md:hidden'>
+                    <AiOutlineMenu size={25}/>
+                </div>
+            </div>
         </div>
-            <ul className='hidden md:flex'>
-                <Link href="/">
-                <li className ='ml10 text sm uppercase hover:border-b' >Home</li>
-                </Link>
-                <Link href="/">
-                <li className ='ml10 text sm uppercase hover:border-b' >About</li>
-                </Link>
-                <Link href="/">
-                <li className ='ml10 text sm uppercase hover:border-b' >Skills</li>
-                </Link>
-                <Link href="/">
-                <li className ='ml10 text sm uppercase hover:border-b' >Projects</li>
-                </Link>
-                <Link href="/">
-                <li className ='ml10 text sm uppercase hover:border-b' >Contact</li>
-                </Link>
-            </ul>
+        <div className='fixed left-0 top-0 w-full h-screen bg-black/70'>
+            <div className='fixed left-0 top-0 w-[75%] sm:w-[60%] md:w[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500'>
+                <div>
+                    <div className='flex w-full items-center justify-between'>
+                        <Image src='/../public/assets/navLogo.png' width ='87' height='35' alt='/'/>
+                        <div className= 'rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer'>
+                            <AiOutlineClose/>
+                        </div>
+                    </div>
+                    <div className='border-b border-gray-300 my-4' >
+                        <p className ='w-[85%] md:w-[90%] py-4'> Let's build something legendary together</p>
+                    </div>
+                </div>
+                
+            </div>
             
-
-
+        </div>
 
     </div>
   )
