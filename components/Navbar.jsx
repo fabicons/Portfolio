a223d7ce38cf3630c83d10127f13ba2424ed800f
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-07-31 14:32:40 +0100

    Navbar: burger menu: completed layout & style

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,9 @@
 import Image from 'next/image'
 import React from 'react'
 import Link from 'next/link'
-import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from 'react-icons/ai'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 const Navbar = () => {
   return (
@@ -46,10 +48,51 @@ const Navbar = () => {
                         </div>
                     </div>
                     <div className='border-b border-gray-300 my-4' >
-                        <p className ='w-[85%] md:w-[90%] py-4'> Let's build something legendary together</p>
+                        <p className ='w-[85%] md:w-[90%] py-4'> 
+                            Let&apos;s build something legendary together
+                        </p>
                     </div>
                 </div>
-                
+                <div className='py-4 flex-col'>
+                    <ul className='uppercase'>
+                        <Link href='/'>
+                            <li className='py-4 text-sm'>Home</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='py-4 text-sm'>About</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='py-4 text-sm'>Skills</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='py-4 text-sm'>Projects</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='py-4 text-sm'>Contact</li>
+                        </Link>
+                    </ul>
+                    <div className='pt-40'>
+                        <p className='uppercase tracking-widest text-[#5651e5]'>
+                            Let&apos;s Connect
+                        </p>
+                        <div className= 'flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                <FaLinkedin />
+                            </div>
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                <FaGithub />
+                            </div>
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                <AiOutlineMail />
+                            </div>
+                            <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                <BsFillPersonLinesFill />
+                            </div>
+                            
+                        </div>
+
+                    </div>
+                </div>
             </div>
             
         </div>
