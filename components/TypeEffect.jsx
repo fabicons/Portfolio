3c11ff3ba618366d124cab03487779c4c37f0970
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-09-23 10:08:59 +0200

    added css text color to typeEffect component

--- components/TypeEffect.jsx
+++ components/TypeEffect.jsx
@@ -3,7 +3,7 @@ import Typewriter from 'typewriter-effect'
 export default function TypeEffect() {
 	return (
 		<div>
-			<h1>
+			<h1 className='text-gray-700 dark:text-gray-200'>
 				<Typewriter
 					options={{
 						strings: [
