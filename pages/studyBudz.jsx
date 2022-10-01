c765f340be52a7779cc97c1e2b1b0dda4fab40f8
ffjaervik <100479956+ffjaervik@users.noreply.github.com>
2022-10-01 11:12:54 +0100

    changed studyBudz page copy

--- pages/studyBudz.jsx
+++ pages/studyBudz.jsx
@@ -18,17 +18,23 @@ function studyBudz() {
 				/>
 				<div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
 					<h1 className='py-2'>StudyBudz</h1>
-				
 				</div>
 			</div>
 			<div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8'>
 				<div className='col-span-4'>
 					<p className='mb-1 dark:text-gray-300'>StudyBudz</p>
 					<h2 className='mb-4'>Overview</h2>
-					<p className='dark:text-gray-300'>
+					<p className='dark:text-gray-300 text-xl'>
 						Study Budz is the result of the mid-term project of the School of
-						Code Bootcamp. The students where given 1 week (Mon - Fri) to come
-						up with a working product that relating to the brief.
+						Code Bootcamp. The students were given one week (Mon - Fri) to come
+						up with a working product in relation to the brief.
+						<br></br>
+						The brief was to come up with a product that enhances coding
+						bootcamp stundent&apos;s life during the intensive course. StudyBudz
+						aims to assist bootcampers by offering additional lessons by
+						students
+						<em> for</em> students, to improve their skills during their own
+						time. Follow the links below to check out the Github repository.
 					</p>
 					<button className='px-8 py-2 mt-4 dark:shadow-gray-500'>
 						{' '}
@@ -49,17 +55,20 @@ function studyBudz() {
 								<RiRadioButtonFill className='pr-1' /> React
 							</p>
 							<p className='py-2 flex items-center'>
-								<RiRadioButtonFill className='pr-1' /> Tailwind
+								<RiRadioButtonFill className='pr-1' /> CSS
 							</p>
 							<p className='py-2 flex items-center'>
 								<RiRadioButtonFill className='pr-1' /> JavaScript
 							</p>
 
 							<p className='py-2 flex items-center'>
-								<RiRadioButtonFill className='pr-1' /> Google API
+								<RiRadioButtonFill className='pr-1' /> Node / Express
 							</p>
 							<p className='py-2 flex items-center'>
-								<RiRadioButtonFill className='pr-1' /> Zillow API
+								<RiRadioButtonFill className='pr-1' /> REST API
+							</p>
+							<p className='py-2 flex items-center'>
+								<RiRadioButtonFill className='pr-1' /> PostgreSQL
 							</p>
 						</div>
 					</div>
